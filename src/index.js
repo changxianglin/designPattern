0b6287559c7dee23dee663f1ba9bbd4b2b5844a2
changxianglin <281532612@qq.com>
2020-03-09 01:45:01 +0800

    UML

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
-class jQuery {
-  constructor(selector) {
-    let slice = Array.prototype.slice
-    let dom = slice.call(document.querySelectorAll(selector))
-    let len = dom ? dom.length : 0
-    for( let i = 0; i < len; i++) {
-      this[i] = dom[i]
-    }
-    this.length = len
-    this.selector = selector || ''
+class People {
+  constructor(name, house) {
+    this.name = name
+    this.house = house
   }
-  append(node) {
 
-  }
-  addClass(name) {
-
-  }
-  html(data) {
-
+  saySomething() {
+    
   }
 }
 
-window.$ = function(selector) {
-  // 工厂模式
-  return new jQuery(selector)
+class A extends People {
+  constructor(name, house) {
+    super(name, house)
+  }
+  saySomething() {
+    alert('I am A')
+  }
 }
 
+class B extends People {
+  constructor(name, house) {
+    super(name, house)
+  }
+  saySomething() {
+    alert('I am B')
+  }
+}
 
-// test
-var $p = $('p')
-console.log($p)
-console.log($p.addClass)
+// UML 图
+// https://www.processon.com
+// MS office visio
+let aHouse = new House('北京')
+let a = new A('a', aHouse)
+a.saySomething()
+let b = new B('b')
+b.saySomething()
