71c04f883f21c1b1b6f0472a77d38e736f8e7d23
changxianglin <281532612@qq.com>
2020-03-17 23:59:44 +0800

    add class

--- src/index.js
+++ src/index.js
@@ -1,37 +1,20 @@
-class Car {
-  constructor(number, name) {
-    this.number = number
-    this.name = name
+class Product {
+  constructor(name) {
+    this.name = name 
+  }
+  init() {
+    alert('init')
+  }
+  fun1() {
+    alert('fun1')
+  }
+  fun2() {
+    alert('fun2')
   }
 }
 
-class Kuaiche extends Car {
-  constructor(number, name) {
-    super(number, name)
-    this.price = 1
+class Creator {
+  create(name) {
+    return new Product()
   }
 }
-
-class Zhuanche extends Car {
-  constructor(number, name) {
-    super(number, name)
-    this.price = 2
-  }
-}
-
-class Trip {
-  constructor(car) {
-    this.car = car
-  }
-  start() {
-    console.log(`行程开始, 车辆名称： ${this.car.name}, 车牌号: ${this.car.number}`)
-  }
-  end() {
-    console.log(`行程结束, 金额： ${this.car.price * 5}`)
-  }
-}
-
-let car = new Kuaiche(100, '大众')
-let trip = new Trip(car)
-trip.start()
-trip.end()
