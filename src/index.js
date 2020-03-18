a63522359c28fb0a818b5e22e2c82e65e03058de
changxianglin <281532612@qq.com>
2020-03-18 23:59:50 +0800

    proxy class

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
-class ReadImg {
-  constructor(fileName) {
-    this.fileName = fileName
-    this.loadFromDisk() // init load file from hard disk, simulation
-  }
-
-  display() {
-    console.log('display...' + this.fileName)
-  }
-
-  loadFromDisk() {
-    console.log('loading...' + this.fileName)
-  }
+// star
+const star = {
+  name: '张xxx',
+  age: 25,
+  phone: '13800001111'
 }
 
-class ProxyImg {
-  constructor(fileName) {
-    this.readImg = new ReadImg(fileName)
+// broker
+let agent = new Proxy(star, {
+  get: function(target, key) {
+    if(key === 'phone') {
+      // broker phone
+      return 'broker: 17889989988'
+    }
+    if(key === 'price') {
+      // broker speak price
+      return 120000
+    }
+    return target[key]
+  },
+  set: function(target, key, val) {
+    if(key === 'customPrice') {
+      if(val < 100000) {
+        // lower 10w
+        throw new Error('to lower price')
+      } else {
+        target[key] = val
+        return true
+      }
+    }
   }
-  display() {
-    this.readImg.display()
-  }
-}
+})
 
 // test
-const proxyImg = new ProxyImg('1.png')
-proxyImg.display()
+console.log(agent.name)
+console.log(agent.age)
+console.log(agent.phone)
+console.log(agent.price)
+
+agent.customPrice = 9000
+console.log(agent.customPrice)
