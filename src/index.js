52357979552ff39f1e3af6e3da1bb6ddd87d0f4f
changxianglin <281532612@qq.com>
2020-03-20 01:52:07 +0800

    iterator

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
-const fs = require('fs')
-const readline = require('readline')
+class Iterator {
+  constructor(container) {
+    this.list = container.list
+    this.index = 0
+  }
+  next() {
+    if(this.hasNext()) {
+      return this.list[this.index++]
+    } else {
+      return null 
+    }
+  }
+  hasNext() {
+    if(this.index >= this.list.length) {
+      return false
+    } else {
+      return true
+    }
+  }
+}
 
-const rl = readline.createInterface({
-  input: fs.createReadStream('../release/bundle.js')
-})
 
-let lineNum = 0
-rl.on('line', function(line) {
-  console.log(lineNum)
-  lineNum++
-})
+class Container {
+  constructor(list) {
+    this.list = list
+  }
 
-rl.on('close', function() {
-  console.log('lineNum', lineNum)
-})
+  // iterator
+  getIterator() {
+    return new Iterator(this)
+  }
+}
+
+// test
+var arr = [1, 2, 3, 4, 5, 6]
+const container = new Container(arr)
+let iterator = container.getIterator()
+while(iterator.hasNext()) {
+  console.log(iterator.next())
+}
