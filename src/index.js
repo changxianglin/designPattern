bec3075fa83755cbf449261b99e48f95996b756b
changxianglin <281532612@qq.com>
2020-03-24 00:42:34 +0800

    prototype

--- src/index.js
+++ src/index.js
@@ -1,78 +1,23 @@
-import StateMachine from 'javascript-state-machine'
-
-const fsm = new StateMachine({
-  init: 'pending',
-  transitions: [
-    {
-      name: 'resolve', // event name
-      from: 'pengding',
-      to: 'fullfiled'
-    },
-    {
-      name: 'reject',
-      from: 'pending',
-      to: 'rejected'
-    }
-  ],
-  methods: {
-    // listen resolve
-    onResolve: function(state, data) {
-      // state
-      data.succesList.forEach(fn => fn())
-    },
-    // listen reject
-    onReject: function(state, data) {
-      data.succesList.forEach(fn => fn())
-    }
-  }
-})
-
-// definition promise
-class myPromise {
-  constructor(fn) {
-    this.succesList = []
-    this.failList = []
-
-    fn(function() {
-      // resolve
-      fsm.resolve(this)
-    }, function() {
-      // reject
-      fsm.reject(this)
-    })
-  }
-  then(succesFn, failFn) {
-    this.succesList.push(succesFn)
-    this.failList.push(failFn)
+// a prototype object
+const prototype = {
+  getName: function() {
+    return this.first + ' ' + this.last
+  },
+  say: function() {
+    alert('hello')
   }
 }
 
-// test
-function loadImg(src) {
-  const promise = new Promise(function(resolve, reject) {
-    let img = document.createElement('img')
-    img.onload = function() {
-      resolve(img)
-    }
-    img.onerror = function() {
-      reject()
-    }
-    img.src = src
-  })
-  return promise
-}
+// base prototype create x
+let x = Object.create(prototype)
+x.first = 'A'
+x.last = 'B'
+alert(x.getName())
+x.say()
 
-let src = 'https://img4.mukewang.com/570cfbfa0001ec3b06400640-100-100.jpg'
-let result = loadImg(src)
-
-result.then(function() {
-  console.log('ok1')
-}, function() {
-  console.log('fail1')
-})
-
-result.then(function() {
-  console.log('ok2')
-}, function() {
-  console.log('fail2')
-})
+// base prototype create y
+let y = Object.create(prototype)
+y.first = 'C'
+y.last = 'D'
+alert(y.getName())
+y.say()
