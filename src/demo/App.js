f6ecd5e917dde04c86b5fc4a48938b8bb3f907e5
changxianglin <281532612@qq.com>
2020-03-28 00:31:03 +0800

    list class

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,9 +1,25 @@
+import $ from 'jquery'
+import ShoppingCart from './ShoppingCart/ShoppingCart'
+import List from './List/List'
+
 export default class App {
   constructor(id) {
-
+    this.$el = $('#' + id)
   }
 
+  initShoppingCart() {
+    let shoppingCart = new ShoppingCart(this)
+    shoppingCart.init()
+  }
+
+  initList() {
+    let list = new List(this)
+    list.init()
+  }
+
+  // init 
   init() {
-    console.log(123)
+    this.initShoppingCart()
+    this.initList()
   }
 }
